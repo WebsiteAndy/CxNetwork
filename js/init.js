44d641cae5bfe788c47d72b51917db2cdfe8d5c4
vagrant <vagrant@scotchbox>
2018-05-16 11:49:19 +0000

    Initial Test Work. Added streams + overlay

---
+++ js/init.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    $(".stream-overlay.ice").click(function() {
+    	alert("hey");
+    	window.location.href= window.location.href + "ice_poseidon.html";
+    })
+});
